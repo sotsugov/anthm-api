2d6881bcdfdea55cea1ef64e0790f574f6cb880d
Igor Sotsugov <234929+sotsugov@users.noreply.github.com>
2024-05-12 16:31:27 +0100

    Add SummaryEvaluation model into Summary

--- app/models/summaries.py
+++ app/models/summaries.py
@@ -2,9 +2,15 @@ from typing import List
 from pydantic import BaseModel
 
 
+class SummaryEvaluation(BaseModel):
+    sentence: str
+    isInferable: bool
+    explanation: str
+
+
 class Summary(BaseModel):
     _id: str
     sessionId: str
     category: str
     content: str
-    evaluation: List[dict]
+    evaluation: List[SummaryEvaluation]
